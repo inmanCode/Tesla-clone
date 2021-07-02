307da741193a0ffe7f25a9d728b1affa25954e34
inmanCode <consolelog031@gmail.com>
2021-07-02 03:06:31 +0200

    -

--- inman-tesla/src/components/Section.jsx
+++ inman-tesla/src/components/Section.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/section.css';
+import Fade from 'react-reveal/Fade';
 
 const Section = ({ title, bgImg, description, filterApplication }) => {
   return (
@@ -7,16 +8,22 @@ const Section = ({ title, bgImg, description, filterApplication }) => {
       className='section-container'
       style={{
         backgroundImage: `url(/images/${bgImg})`,
-        filter: filterApplication ? `blur(4px)` : ``,
+        filter: filterApplication ? `blur(4px)` : `blur(0px)`,
       }}
     >
       <div className='car-info'>
-        <h1>{title}</h1>
-        <p>{description}</p>
+        <Fade bottom>
+          <h1>{title}</h1>
+          <p>{description}</p>
+        </Fade>
       </div>
       <div className='btns'>
-        <div className='left-btn'>Costum order</div>
-        <div className='right-btn'>Existing inventory</div>
+        <Fade left>
+          <div className='left-btn'>Costum order</div>
+        </Fade>
+        <Fade right>
+          <div className='right-btn'>Existing inventory</div>
+        </Fade>
       </div>
       <img src='/images/down-arrow.svg' alt={title} className='down-arrow' />
     </div>
